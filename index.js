146358300171a66af0f88e1f8db219ed925f16e2
Bae SangHyuk <44903787+D3vle0@users.noreply.github.com>
2020-12-27 23:23:25 +0900

    add register backend

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var sha256 = require('sha256');
-var app = express();
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const sha256 = require('sha256');
+const fs = require('fs');
+const app = express();
 
 var PORT = process.env.PORT || 5000;
 
@@ -17,33 +18,62 @@ app.use(session({
 }));
 
 app.post('/', function (req, res) {
-    var pwDB = "c9a4f74de4b37231fa2ba2cfb6760418551e7d382d160a71a9b540ec1c971f89";
-    var pwd = req.body.password;
+    const db = JSON.parse(fs.readFileSync('db.json'));
+    var id = req.body.id;
+    var pw = req.body.pw;
 
-    if (sha256(`${pwd}thsy$S&mw3%ANW#^N&5us`) === pwDB) {
-        req.session.displayName = 'Ang';
+    var idx = db.id.findIndex((e) => e == id);
+
+    if (idx !== -1 && sha256(pw) === db.pw[idx]) {
+        req.session.displayName = `logged-in:${id}`;
         res.redirect('/');
     }
     else {
         delete req.session.displayName;
-        res.render("pw_error");
+        res.render("login_err");
     }
 });
 
 app.get('/', function (req, res) {
     if (req.session.displayName) {
+        //const study_db = JSON.parse(fs.readFileSync('study_db.json'));
         res.render("index");
-    } else {
+    }
+    else {
         res.render("login");
     }
 });
 //임시
-app.get('/index', function (req, res) {
-    res.render("index");
-});
+// app.get('/index', function (req, res) {
+//     res.render("index");
+// });
 
 app.get('/register', function (req, res) {
-    res.render("register");
+    res.render("register", { errmsg: "" });
+});
+
+app.post('/register', function (req, res) {
+    const db = JSON.parse(fs.readFileSync('db.json'));
+    var new_id = req.body.id;
+    var new_pw = req.body.pw;
+
+    var idx = db.id.findIndex((e) => e == new_id);
+    if (idx !== -1) {
+        res.render('register', { errmsg: "이미 존재하는 아이디 입니다." });
+    }
+    else {
+        const db = JSON.parse(fs.readFileSync('db.json'));
+        db.id.push(new_id);
+        db.pw.push(sha256(new_pw));
+        fs.writeFile("db.json", JSON.stringify(db), err => {
+            if (err)
+                throw err;
+            else {
+                console.log(`${new_id} added!`);
+                res.redirect("/");
+            }
+        });
+    }
 });
 
 app.listen(PORT, function () {
